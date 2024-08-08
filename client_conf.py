25f9472a34adf58d0f570398592d8e7ee792e625
root <titas20031996@gmail.com>
2024-08-08 05:54:40 -0400

    test commit 3

--- client_conf.py
+++ client_conf.py
@@ -1,4 +1,4 @@
 import logcliasst as assist
 print(assist.client_ruleset_config("test_rule","localhost",524,"tcp"))
-#print(assist.apply_ruleset_client("/var/log/message.log","test","local3","info","test_rule"))
-#print(assist.client_tls_template("/etc/cert.pem","localhost","tcp",524,"test_rule"))
+print(assist.apply_ruleset_client("/var/log/message.log","test","local3","info","test_rule"))
+print(assist.client_tls_template("/etc/cert.pem","localhost","tcp",524,"test_rule"))
